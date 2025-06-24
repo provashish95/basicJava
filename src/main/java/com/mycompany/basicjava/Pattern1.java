756236e30285162626037a91e6593f11fb6eba7e
provashish95 <roytonmoy95@gmail.com>
2025-06-24 12:51:25 +0600

    Pattern draw

---
+++ src/main/java/com/mycompany/basicjava/Pattern1.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.basicjava;
+import java.util.Scanner; 
+
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+        
+        System.out.println("Please enter line numbers: ");
+        int n = input.nextInt(); 
+        
+        for(int row = 1; row <= n; row++){
+            for(int col = 1; col <=row; col++){
+                //System.out.print(col);
+                //System.out.print("#");
+                //System.out.print("*");
+                System.out.print((char)('a' + col - 1));
+            }
+            System.out.println(" ");
+        }   
+    }
+}
---
+++ src/main/java/com/mycompany/basicjava/Pattern2.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.basicjava;
+import java.util.Scanner; 
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+        
+        System.out.println("Enter line numbers: ");
+        int n = input.nextInt(); 
+        
+        for(int row = n; row >= 1; row--){
+            for(int col = 1; col <= row; col++){
+                //System.out.print(col%2);
+                //System.out.print(row);
+                //System.out.print(row%2);
+                System.out.print(col);
+            }
+            System.out.println("");
+        }
+    }
+}
