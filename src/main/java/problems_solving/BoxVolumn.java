b1ad9b59ea9d35619d631a77b9ad4c6cb5a947dc
provashish95 <roytonmoy95@gmail.com>
2025-06-29 16:50:15 +0600

    Problem Solving

---
+++ src/main/java/problems_solving/BoxVolumn.java
@@ -0,0 +1,12 @@
+package problems_solving;
+
+public class BoxVolumn {
+    public static void main(String[] args) {
+        
+        Box box1 = new Box(10,10,10); 
+        Box box2 = new Box(20,23,10); 
+
+        box1.displayVol();
+        box2.displayVol();
+    }
+}
