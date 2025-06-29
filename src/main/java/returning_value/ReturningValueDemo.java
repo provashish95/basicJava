95db42297395a49b188923d86c5af6062c61c5d3
provashish95 <roytonmoy95@gmail.com>
2025-06-29 15:03:05 +0600

    return value

---
+++ src/main/java/returning_value/ReturningValueDemo.java
@@ -0,0 +1,9 @@
+
+package returning_value;
+
+public class ReturningValueDemo {
+    
+    int square(int value){
+        return value*value; 
+    }
+}
---
+++ src/main/java/returning_value/Test.java
@@ -0,0 +1,14 @@
+
+package returning_value;
+
+public class Test {
+    public static void main(String[] args) {
+        
+        ReturningValueDemo ob1 = new ReturningValueDemo();  
+    
+       // int result = ob1.square(10); 
+       // System.out.println("Square is: "+ result);
+       
+        System.out.println("Square is: "+ ob1.square(10));
+    }
+} 
