359bf99a754b3429cbed0e33421101a713da5bd5
provashish95 <roytonmoy95@gmail.com>
2025-06-29 16:54:02 +0600

    This keyword

--- src/main/java/problems_solving/Box.java
+++ src/main/java/problems_solving/Box.java
@@ -3,10 +3,18 @@ package problems_solving;
 public class Box {
     double height, width, depth; 
     
-    Box(double h, double w, double d){
-        height = h; 
-        width = w; 
-        depth = d; 
+//    Box(double h, double w, double d){
+//        height = h; 
+//        width = w; 
+//        depth = d; 
+//    }
+    
+    
+    //This keyword...
+    Box(double height, double width, double depth){
+        this.height = height; 
+        this.width = width; 
+        this.depth = depth; 
     }
     
     void displayVol(){
