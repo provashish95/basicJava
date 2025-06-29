b4d9f26b309dcb6de440ec28426ef7a8fefae891
provashish95 <roytonmoy95@gmail.com>
2025-06-29 15:45:41 +0600

    Static variable

--- src/main/java/static_variable/StaticVariable.java
+++ src/main/java/static_variable/StaticVariable.java
@@ -5,4 +5,14 @@ package static_variable;
 public class StaticVariable {
     
    static String universityName = "DIU"; 
+   static int count = 0; 
+   
+   StaticVariable(){
+       count++; 
+   }
+   
+   void display(){
+       System.out.println("Student: " + count);
+   }
+   
 }
--- src/main/java/static_variable/StaticVariableTest.java
+++ src/main/java/static_variable/StaticVariableTest.java
@@ -3,9 +3,18 @@ package static_variable;
 public class StaticVariableTest {
 
     public static void main(String[] args) {
+
+        // StaticVariable ob = new StaticVariable();
+        System.out.println("University Name: " + StaticVariable.universityName);
+
+        StaticVariable student1 = new StaticVariable();
+        student1.display();
+
+        StaticVariable student2 = new StaticVariable();
+        student2.display();
         
-       // StaticVariable ob = new StaticVariable();
-        
-        System.out.println("University Name: "+ StaticVariable.universityName);
+        StaticVariable student3 = new StaticVariable();
+        student3.display();
+
     }
 }
