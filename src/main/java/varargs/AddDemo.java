b1ec64c8187c8e7235203b26fe17071944dd4231
provashish95 <roytonmoy95@gmail.com>
2025-06-30 10:58:02 +0600

    method parameter length

---
+++ src/main/java/varargs/AddDemo.java
@@ -0,0 +1,13 @@
+package varargs;
+
+public class AddDemo {
+
+    void add(int... num) {
+        int sum = 0;
+        for (int x : num) {
+            sum += x;
+            System.out.println(x);
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/varargs/AddDemoTest.java
@@ -0,0 +1,11 @@
+package varargs;
+
+public class AddDemoTest {
+
+    public static void main(String[] args) {
+        AddDemo ob = new AddDemo();
+        //ob.add(10,20);
+        ob.add(10, 20, 30);
+
+    }
+}
