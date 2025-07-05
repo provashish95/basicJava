1d641da4395ff2796408eda1ae90a1531ced27b0
provashish95 <roytonmoy95@gmail.com>
2025-07-05 17:03:54 +0600

    Updated

---
+++ src/main/java/assignment/CountOccurrences.java
@@ -0,0 +1,17 @@
+/*
+ 5. numbers[]={1,2,2,3,3,3,4,4,4,4} Count the occurrence of each digits: 
+Output:
+1 1
+2 2
+3 3
+4 4
+ */
+package assignment;
+
+/**
+ *
+ * @author USER
+ */
+public class CountOccurrences {
+    
+}
