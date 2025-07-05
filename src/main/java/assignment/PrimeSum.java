28aef93edca5e3262c23c22d23f7e6282af732b7
provashish95 <roytonmoy95@gmail.com>
2025-07-05 16:52:07 +0600

    Sum of prime numbers

---
+++ src/main/java/assignment/PrimeSum.java
@@ -0,0 +1,33 @@
+/*
+ Given array: numbers[] = {11,7,2,15,6} In the above array, sum of the prime numbers only. Output: 18
+ */
+package assignment;
+
+public class PrimeSum {
+    public static void main(String[] args) {
+         int[] numbers = {11, 7, 2, 15, 6};
+        int sum = 0;
+        
+        for(int i =0; i < numbers.length; i++){
+            int n = numbers[i]; 
+            boolean isPrime = true; 
+            
+            if(n < 2){
+                isPrime = false; 
+            }else{
+                for(int j = 2; j <= Math.sqrt(n); j++){
+                    if(n % j == 0){
+                        isPrime = false; 
+                        break; 
+                    }
+                }
+            }
+            if(isPrime){
+            sum = sum + n; 
+        }
+        }
+        System.out.println(" Sum of Prime number is: " + sum);
+        
+    }
+ 
+}
