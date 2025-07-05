46476a94a9226408864137a2530f2095e39f4a84
provashish95 <roytonmoy95@gmail.com>
2025-07-05 17:07:31 +0600

    Problem solving

--- src/main/java/assignment/CountOccurrences.java
+++ src/main/java/assignment/CountOccurrences.java
@@ -8,10 +8,26 @@ Output:
  */
 package assignment;
 
-/**
- *
- * @author USER
- */
+
 public class CountOccurrences {
-    
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        int[] count = new int[max + 1];
+        for (int num : numbers) {
+            count[num]++;
+        }
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > 0) {
+                System.out.println(i + " " + count[i]);
+            }
+        }
+    }
 }
