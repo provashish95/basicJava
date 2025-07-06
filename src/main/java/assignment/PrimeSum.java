eb222be04925d4286287ab12a1c621ad75459092
provashish95 <roytonmoy95@gmail.com>
2025-07-06 21:14:30 +0600

    Update

--- src/main/java/assignment/PrimeSum.java
+++ src/main/java/assignment/PrimeSum.java
@@ -4,30 +4,31 @@
 package assignment;
 
 public class PrimeSum {
+
     public static void main(String[] args) {
-         int[] numbers = {11, 7, 2, 15, 6};
+        int[] numbers = {11, 7, 2, 15, 6};
         int sum = 0;
-        
-        for(int i =0; i < numbers.length; i++){
-            int n = numbers[i]; 
-            boolean isPrime = true; 
-            
-            if(n < 2){
-                isPrime = false; 
-            }else{
-                for(int j = 2; j <= Math.sqrt(n); j++){
-                    if(n % j == 0){
-                        isPrime = false; 
-                        break; 
+
+        for (int i = 0; i < numbers.length; i++) {
+            int n = numbers[i];
+            boolean isPrime = true;
+
+            if (n < 2) {
+                isPrime = false;
+            } else {
+                for (int j = 2; j <= Math.sqrt(n); j++) {
+                    if (n % j == 0) {
+                        isPrime = false;
+                        break;
                     }
                 }
             }
-            if(isPrime){
-            sum = sum + n; 
-        }
+            if (isPrime) {
+                sum = sum + n;
+            }
         }
         System.out.println(" Sum of Prime number is: " + sum);
-        
+
     }
- 
+
 }
