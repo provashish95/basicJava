4622a93705157e3bfceb61cdd45abcdb64ebb4c0
provashish95 <roytonmoy95@gmail.com>
2025-08-17 23:38:54 +0600

    ramdom password generate

--- src/main/java/MyString/randomPassword.java
+++ src/main/java/MyString/randomPassword.java
@@ -1,5 +1,25 @@
 package MyString;
 
-public class randomPassword {
+import java.util.Random;
 
+public class randomPassword {
+    public static void main(String[] args) {
+      String password =  generateRandomPassword();
+        System.out.println(password);
+    }
+
+    public static String generateRandomPassword(){
+        String letters = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ1234567890?&*";
+        char[] chars = letters.toCharArray();
+
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i <  8; i++){
+           int randomIndex =  random.nextInt(chars.length);
+           //System.out.println(letters.charAt(randomIndex));
+            sb.append(letters.charAt(randomIndex));
+        }
+        return sb.toString();
+    }
 }
